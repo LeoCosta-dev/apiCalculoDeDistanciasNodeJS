b67d964a56dbc92b7bcb85d727db15d4fdd5e3e2
LeoCosta-dev <leonardodacostadasilva@hotmail.com>
2021-11-19 21:45:34 -0300

    aidçao de modulo cors

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 const customExpress = require("./config/customExpress");
+const cors = require("cors")
 const app = customExpress();
 const PORT = process.env.PORT || 3000;
-
+app.use(cors())
 
 app.listen(PORT, () => {
     console.log(`Servido rodadando na porta ${port}.`)
