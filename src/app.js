6833a271fdb356228aa775009c1f1bc7fb017daf
LeoCosta-dev <leonardodacostadasilva@hotmail.com>
2021-11-19 21:19:47 -0300

    adiçao da variavel de porta

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 const customExpress = require("./config/customExpress");
 const app = customExpress();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 
-app.listen(port, () => {
+app.listen(PORT, () => {
     console.log(`Servido rodadando na porta ${port}.`)
 })
